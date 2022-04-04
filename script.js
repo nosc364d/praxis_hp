b7e79881a7b69f12f314c7138733aaf14039181d
Nora Schäfer <nora.schaefer@mailbox.tu-dresden.de>
2022-04-04 18:01:47 +0200

    Fixed header image issue

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function changeSvgColor() {
-    var el = document.querySelectorAll(".line", ".circle");
+    let el = document.querySelectorAll(".line", ".circle");
     el.setAttribute("fill", "var(--secondary);")
+}
+
+function toggleMenu() {
+    let el = document.querySelectorAll("#navbar");
+    el.classList.toggle("visible");
+    let icon = document.querySelector("#menuButton");
+    icon.classList.toggle("visible");
 }
